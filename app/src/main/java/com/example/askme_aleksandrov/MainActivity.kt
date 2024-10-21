6bddefac907013d00b05107374f3a18bc14baa14
Aleksandrov Aleksey <aaaleksandrov09@gmail.com>
2024-10-21 23:52:53 +0300

    init

---
+++ app/src/main/java/com/example/askme_aleksandrov/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.askme_aleksandrov
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        }
+    }
